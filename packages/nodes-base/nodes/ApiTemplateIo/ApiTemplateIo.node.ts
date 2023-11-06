d8d8a1df6d3f7ac0a776eb99c7193f68bfe9fced
Omri Attoun <omriattoun@gmail.com>
2023-11-06 12:20:23 -0800

    apiTemplate - set cloud variable to 1

--- packages/nodes-base/nodes/ApiTemplateIo/ApiTemplateIo.node.ts
+++ packages/nodes-base/nodes/ApiTemplateIo/ApiTemplateIo.node.ts
@@ -488,6 +488,7 @@ export class ApiTemplateIo implements INodeType {
 
 						const qs = {
 							template_id: this.getNodeParameter('pdfTemplateId', i),
+							cloud: 1,
 						};
 
 						let data;
